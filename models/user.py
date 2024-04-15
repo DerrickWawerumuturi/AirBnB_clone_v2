0ce57d31d423713171b116e2715715cc4a48f6ae
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-04-15 16:04:15 +0300

    u

--- models/user.py
+++ models/user.py
@@ -3,6 +3,9 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
