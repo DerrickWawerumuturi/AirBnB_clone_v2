12beffee63f52295b418a812eb51251f455adf5b
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-04-15 16:05:12 +0300

    user

--- models/user.py
+++ models/user.py
@@ -2,17 +2,23 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from models.place import Place
 from models.review import Review
 
+
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    
-    """ NEW ATTRS"""
+    # email = ''
+    # password = ''
+    # first_name = ''
+    # last_name = ''
+
+    """New attrs"""
     __tablename__ = "users"
     email = Column(String(length=128), nullable=False)
     password = Column(String(length=128), nullable=False)
-    attribute = Column(String(length=128), nullable=False)
+    first_name = Column(String(length=128), nullable=False)
     last_name = Column(String(length=128), nullable=False)
+    places = relationship("Place", backref="user", cascade="all, delete")
+    reviews = relationship("Review", backref="user", cascade="all, delete")
