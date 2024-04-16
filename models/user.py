b00950cac84613d1a99eebd2e9e05655384ba44b
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2024-04-16 15:02:37 +0300

    sp

--- models/user.py
+++ models/user.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
-
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
-    email = Column('email',String(128), nullable=False)
-    password = Column('password',String(128), nullable=False)
-    attribute = Column('attribute',String(128), nullable=False)
-    first_name = Column('first_name',String(128), nullable=False)
-    last_name = Column('last_name',String(128), nullable=False)
-    places = relationship('Place', backref='user',cascade='all, delete')
+    email = Column('email', String(128), nullable=False)
+    password = Column('password', String(128), nullable=False)
+    attribute = Column('attribute', String(128), nullable=False)
+    first_name = Column('first_name', String(128), nullable=False)
+    last_name = Column('last_name', String(128), nullable=False)
+    places = relationship('Place', backref='user', cascade='all, delete')
     reviews = relationship('Review', backref='user', cascade="all, delete")
