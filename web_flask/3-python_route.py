165b3a6ca498935c3f5d82bde672964e49ec4c68
Derrick Muturi <wawerumuturi57@gmail.com>
2024-05-17 14:12:41 +0300

    /python/<text>

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"routes to / and  /hbnb"
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get('/', strict_slashes=False)
+def hello():
+    """
+    string to return when on / route
+    """
+    return "Hello HBNB!"
+
+@app.get('/hbnb')
+def hello_world():
+    """
+    show  on /hbnb
+    """
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def variable(text):
+    """
+    replace _ from the text with a space 
+    return C together with the next
+    """
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def text(text="is cool"):
+    """
+    replace _ in the text with a space
+    default of text is 'is cool'
+    Return: Python <text>
+    """
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
