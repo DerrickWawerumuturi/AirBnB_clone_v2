410be99ddbe39fa843774885823f75604df6606c
Derrick Muturi <wawerumuturi57@gmail.com>
2024-05-17 14:38:28 +0300

    is a number

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -40,13 +40,12 @@ def text(text="is cool"):
     text = text.replace('_', ' ')
     return f"Python {text}"
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """
     display "n" if n is a integer
     """
-    if type(n) == int:
-        return "n"
+    return f"{n} is a number"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
