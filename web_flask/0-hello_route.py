8697c811e9680bc8bb7e1dd1815cd7126180ac12
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-05-17 16:38:41 +0300

    0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
-""" 
-first flask web application
 """
+First Flask web application module
+This module creates a simple Flask web application that returns
+a "Hello HBNB!" message on the home page.
+"""
+
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_world():
-    """ 
-    server as home page
     """
-    return ('Hello HBNB!')
+    Serves as the home page
+    Returns:
+        str: The string "Hello HBNB!"
+    """
+    return 'Hello HBNB!'
 
 if __name__ == "__main__":
-    """ 
-    running my app 
+    """
+    Running the Flask application
     """
     app.run(host='0.0.0.0', port=5000)
 
