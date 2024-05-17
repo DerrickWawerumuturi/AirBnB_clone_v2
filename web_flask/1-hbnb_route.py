664dc22d4c94d3cbd3aa4036dce04f14b8c55c72
Derrick Muturi <wawerumuturi57@gmail.com>
2024-05-17 13:38:59 +0300

    1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"routes to / and  /hbnb"
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get('/', strict_slashes=False)
+def hello():
+    """
+    string to return when on / route
+    """
+    return "Hello HBNB!"
+
+@app.get('/hbnb')
+def hello_world():
+    """
+    show  on /hbnb
+    """
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
