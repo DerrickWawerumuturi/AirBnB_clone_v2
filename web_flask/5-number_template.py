93847f7af5c9c4a1d41b9fe1658fa534f07e8a01
Derrick Muturi <wawerumuturi57@gmail.com>
2024-05-17 14:55:43 +0300

    render_template()

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -52,6 +52,6 @@ def number_template(n):
     """
     render html if n is an integer
     """
-    return render_template('index.html', n)
+    return render_template('5-number.html', number=n)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
